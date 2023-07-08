e774ab00471279a69c44bddadda8e4b90f0a3c0d
Youssef El Saadany <youssef.saadany@gmail.com>
2023-07-08 10:57:14 +0200

    added bio, links and improved style

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -39,7 +39,7 @@ export default async function Page({
   params: { username: string };
 }) {
   const { id, profile } = await getData(params.username);
-  const { full_name, hd_profile_pic_versions } = profile;
+  const { full_name, hd_profile_pic_versions, biography, bio_links } = profile;
   const picture =
     hd_profile_pic_versions.length > 1
       ? hd_profile_pic_versions[1]
@@ -49,33 +49,43 @@ export default async function Page({
     <div>
       {id !== "-1" ? (
         <div>
-          <div className="flex flex-col md:flex-row md:w-fit m-auto">
-            <Image
-              src={`https://quickchart.io/qr?text=https://www.threads.net/@${params.username}&dark=EA7C14&margin=1&format=png&ecLevel=H&size=400`}
-              alt="profile pic"
-              width={300}
-              height={300}
-              className="w-[65%] md:w-[300px] m-auto md:m-0"
-            />
-            <div className="mt-3">
+          <div className="p-2 md:border border-grey-500 rounded-md mt-3 shadow-lg w-11/12 md:w-fit m-auto">
+            <div className="flex flex-col md:flex-row md:w-fit m-auto">
               <Image
-                src={picture.url}
+                src={`https://quickchart.io/qr?text=https://www.threads.net/@${params.username}&dark=EA7C14&margin=1&format=png&ecLevel=H&size=400`}
                 alt="profile pic"
-                width={picture.width}
-                height={picture.height}
-                className="rounded-full w-[65%] m-auto"
+                width={300}
+                height={300}
+                className="w-[65%] md:w-[300px] m-auto md:m-0"
               />
-              <p className="text-2xl text-center w-10/12 m-auto">
-                {profile.full_name}
-              </p>
-              <p
-                className={`text-center m-auto ${
-                  full_name ? "text-xl" : "text-2xl mt-3"
-                }`}
-              >
-                @{params.username}
-              </p>
+              <div className="mt-3">
+                <Image
+                  src={picture.url}
+                  alt="profile pic"
+                  width={picture.width}
+                  height={picture.height}
+                  className="rounded-full w-[65%] m-auto"
+                />
+                <p className="text-2xl text-center w-10/12 m-auto">
+                  {profile.full_name}
+                </p>
+                <p
+                  className={`text-center m-auto ${
+                    full_name ? "text-xl" : "text-2xl mt-3"
+                  }`}
+                >
+                  @{params.username}
+                </p>
+              </div>
             </div>
+            <p className="whitespace-pre-wrap mt-3 w-fit m-auto">{biography}</p>
+            {bio_links.map((link) => (
+              <p className="mt-3 w-fit m-auto text-blue-500 underline">
+                <Link href={link.url}>
+                  {link.url.split("/").slice(2).join("/")}
+                </Link>
+              </p>
+            ))}
           </div>
         </div>
       ) : (
